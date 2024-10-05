00053459e3fe2dc028e0df0569530d28bc8d5c30
taiypeo <Qwertygid@gmail.com>
2024-10-05 16:43:06 +0300

    Return embeddings instead of faiss index

--- kgc_plm/embeddings/sbert.py
+++ kgc_plm/embeddings/sbert.py
@@ -1,10 +1,10 @@
-from faiss import Index, IndexFlatIP
+import numpy as np
 from sentence_transformers import SentenceTransformer
 
 from ..graphs import BaseGraph
 
 
-def embed_sbert(model_name: str, graph: BaseGraph, batch_size: int = 32, cache_dir: str = "cache") -> Index:
+def embed_sbert(model_name: str, graph: BaseGraph, batch_size: int = 32, cache_dir: str = "cache") -> np.ndarray:
     model = SentenceTransformer(model_name, cache_folder=cache_dir)
     embeddings = model.encode(
         sentences=graph.texts(),
@@ -12,7 +12,4 @@ def embed_sbert(model_name: str, graph: BaseGraph, batch_size: int = 32, cache_d
         normalize_embeddings=True,
         show_progress_bar=True,
     )
-
-    index = IndexFlatIP(embeddings.shape[1])
-    index.add(embeddings)
-    return index
+    return embeddings

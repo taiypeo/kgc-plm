a4221afbb10d81829efdc0af71520f427aea784b
taiypeo <Qwertygid@gmail.com>
2025-01-08 17:14:33 +0300

    Fix

--- kgc_plm/graphs/wn18rr.py
+++ kgc_plm/graphs/wn18rr.py
@@ -51,37 +51,37 @@ class WN18RR(BaseGraph):
 
         dataset = {}
         for split_filename in ["train", "valid", "test"]:
+            split = {
+                "head": [],
+                "relation": [],
+                "tail": [],
+            }
             with open(data_path + "/" + split_filename + ".txt") as file:
-                def _generate_split():
-                    for line in file:
-                        head, relation, tail = line.strip().split("\t")
-                        if head[0] == "'":
-                            head = head[1:]
-                        if tail[0] == "'":
-                            tail = tail[1:]
-                        head = head.strip()
-                        relation = relation.strip()
-                        tail = tail.strip()
+                for line in file:
+                    head, relation, tail = line.strip().split("\t")
+                    if head[0] == "'":
+                        head = head[1:]
+                    if tail[0] == "'":
+                        tail = tail[1:]
+                    head = head.strip()
+                    relation = relation.strip()
+                    tail = tail.strip()
 
-                        entity_names.add(head)
-                        entity_names.add(tail)
-                        relation_names.add(relation)
+                    entity_names.add(head)
+                    entity_names.add(tail)
+                    relation_names.add(relation)
 
-                        yield {
-                            "head": head,
-                            "relation": relation,
-                            "tail": tail,
-                        }
+                    split["head"].append(head)
+                    split["relation"].append(relation)
+                    split["tail"].append(tail)
 
-                        if add_reverse_relations:
-                            yield {
-                                "head": tail,
-                                "relation": relation,
-                                "tail": head,
-                            }
+                    if add_reverse_relations:
+                        split["head"].append(head)
+                        split["relation"].append(f"{relation}_reverse")
+                        split["tail"].append(tail)
 
-                split = Dataset.from_generator(_generate_split, cache_dir=cache_dir)
-                dataset[split_filename if split_filename != "valid" else "validation"] = split
+            split = Dataset.from_dict(split, cache_dir=cache_dir)
+            dataset[split_filename if split_filename != "valid" else "validation"] = split
 
         self._entity_names = sorted(entity_names)
         self._relations = sorted(relation_names)
